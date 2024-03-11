f6f0588f32cbac2299dca19d5812404d90e111b9
ShizuokaTerrier <matt@squareoneJapan.com>
2024-03-11 16:10:34 +0900

    Feat: update to schema for admin page

---
+++ prisma/migrations/20240311022007_008_eighth_migration/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "employees" ADD COLUMN     "address_1" VARCHAR(50),
+ADD COLUMN     "address_2" VARCHAR(50),
+ADD COLUMN     "city" VARCHAR(30),
+ADD COLUMN     "country" VARCHAR(30),
+ADD COLUMN     "zipcode" VARCHAR(10);
