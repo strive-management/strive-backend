2ba25ac93d1d78ff8e20f0e8b6b9177dd54621c3
ShizuokaTerrier <matt@squareoneJapan.com>
2024-03-21 18:25:22 +0900

    feat:sepDatastep4 security

--- src/hr_employees/hr_employees.model.ts
+++ src/hr_employees/hr_employees.model.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getEmployees = async (userId: string) => {
+  if (!userId) return 'You are not authorized to access this data';
   const users = await prisma.employees.findMany({
     where: {
       user_id: userId,
@@ -41,6 +42,7 @@ export const patchEmployeeById = async (id: number, data: any) => {
 };
 
 export const getSomeEmployees = async (userId: string) => {
+  if (!userId) return 'You are not authorized to access this data';
   const users = await prisma.employees.findMany({
     where: {
       user_id: userId,
@@ -62,6 +64,7 @@ export const getSomeEmployees = async (userId: string) => {
 };
 
 export const employeeCount = async (userId: string) => {
+  if (!userId) return 'You are not authorized to access this data';
   const employeeNumbers = await prisma.employees.count({
     where: {
       user_id: userId,
@@ -71,6 +74,7 @@ export const employeeCount = async (userId: string) => {
 };
 
 export const onlyYourEmployees = async (userId: string) => {
+  if (!userId) return 'You are not authorized to access this data';
   const userInfoOnly = await prisma.employees.findMany({
     where: {
       user_id: userId,
