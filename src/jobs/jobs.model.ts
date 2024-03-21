322cf77c9e646ba10aea3d8a2f8f5d3fec2159bf
ShizuokaTerrier <matt@squareoneJapan.com>
2024-03-21 19:34:35 +0900

    feat: sep data big update

--- src/jobs/jobs.model.ts
+++ src/jobs/jobs.model.ts
@@ -2,28 +2,29 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export const getJobs = async () => {
+export const getJobs = async (userId: string) => {
+  if (!userId) return [];
   const jobs = await prisma.jobs.findMany();
   return jobs;
 };
 
 export const getJobById = async (id: number) => {
   const job = await prisma.jobs.findUnique({
-    where: { id }
+    where: { id },
   });
   return job;
 };
 
 export const addNewJob = async (data: any) => {
   const job = await prisma.jobs.create({
-    data
+    data,
   });
   return job;
 };
 
 export const deleteJob = async (id: number) => {
   const job = await prisma.jobs.delete({
-    where: { id }
+    where: { id },
   });
   return job;
 };
@@ -31,7 +32,7 @@ export const deleteJob = async (id: number) => {
 export const patchJobById = async (id: number, data: any) => {
   const job = await prisma.jobs.update({
     where: { id },
-    data
+    data,
   });
   return job;
 };
